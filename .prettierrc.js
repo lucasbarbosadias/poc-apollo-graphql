0031a4017ac4a64321bcb220f5f0d9e5d86b85bf
Lucas Dias <lucasbarbosadias@outlook.com>
2024-02-03 20:27:25 -0300

    feat: add prettier and codegen files

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+export default {
+  bracketSpacing: true,
+  singleQuote: true,
+  semi: false,
+  printWidth: 90,
+  endOfLine: 'auto',
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  tabWidth: 2,
+}
